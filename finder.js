349bf57e7d139affdf1a8bd8c054b5469eaf8487
Obaid Sajjad <obaidsajjad321@gmail.com>
2024-12-11 17:26:16 +0500

    second

--- finder.js
+++ finder.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const {ObjectId} = require("mongodb")
 app.use(express.json())
 require('dotenv').config()
